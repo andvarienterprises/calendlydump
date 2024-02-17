d1c2ec800af2a72689a8d723dae8ffde6e238d91
Dave O'Connor <git@mail.andvari.net>
2024-02-17 15:01:14 +0000

    Only spit out a csv

--- calendlydump.py
+++ calendlydump.py
@@ -1,23 +1,12 @@
-"""Check calendly appointments against a janky spreadsheet."""
+"""Dump Calendly appointments to stdout"""
 import datetime
 import requests
 from absl import app
 from absl import flags
-from googleapiclient.discovery import build 
-from google_auth_oauthlib.flow import InstalledAppFlow
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string("calendly_keyfile", "calendly.key",
                     "Patch to Calendly token file")
-flags.DEFINE_string("sheets_keyfile", "sheets.key.json",
-                    "Patch to oauth 2.0 json keyfile")
-flags.DEFINE_string("sheet_id", "",
-                    "Google Sheet ID")
-
-flags.DEFINE_string("email_range", "C2:C100",
-                    "Range for emails")
-flags.DEFINE_string("state_range", "I2:I100",
-                    "range for state")
 
 class CalendlyError(Exception):
     """Generic Error when talking to Calendly."""
@@ -43,66 +32,47 @@ class Calendly(object):
             raise CalendlyError("no resource: " + str(me))
         return me["resource"]
 
-    def get_appointments(self):
-        """ Return a dict of email to datetime of next appointments."""
+    def _get_appts(self, params):
         me = self.me()
-
-        params = {
+        params.update({
             "count": 100,
             "status": "active",
             "user": me["uri"],
-            "min_start_time": datetime.datetime.now(
-                tz=datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%S%z")
-        }
+        })
         events = self.get("scheduled_events", params)
         if "collection" not in events:
             raise CalendlyError("no collection: " + str(events))
         appts = {}
         for e in events["collection"]:
-            start = datetime.datetime.fromisoformat(e["start_time"])
             uuid = e["uri"].split('/')[-1]
-            ppl = self.get("scheduled_events/" + uuid + "/invitees")
-            for p in ppl["collection"]:
-                if p["email"] in appts:
-                    print(f"More than one appointment for {p['email']}")
-                    # Keep the soonest date
-                    if start > appts[p["email"]]:
-                        continue
-                appts[p["email"]] = e["start_time"]
-
+            etype = e["event_type"].split('/')[-1]
+            ppl = self.get("scheduled_events/" + uuid + "/invitees")["collection"]
+            appts[uuid] = (ppl[0]["email"], etype, e["start_time"])
         return appts
 
-class Sheet(object):
-    def __init__(self, keyfile, sheet_id):
-        self._sheet_id = sheet_id
-        self._key = keyfile
-        self._flow = InstalledAppFlow.from_client_secrets_file(
-            keyfile, ["https://www.googleapis.com/auth/spreadsheets"])
-        self._flow.run_local_server()
-        self._creds = self._flow.credentials
-    
-    def get_range(self, range):
-        sheets_svc = build('sheets', 'v4', credentials=self._creds)
-        return sheets_svc.spreadsheets().values().get(
-            spreadsheetId=self._sheet_id, range=range).execute()
+    def get_all_appointments(self):
+        """ Return All Appointments."""
+        return self._get_appts({})
+
+    def get_future_appointments(self):
+        """ Return a dict of email to datetime of next appointments."""
+        params = {
+            "min_start_time": datetime.datetime.now(
+                tz=datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%S%z")
+        }
+        return self._get_appts(params)
 
 def main(_):
     """Do the Thing."""
-    print("Hello.")
     calendly_api_key = open(
         FLAGS.calendly_keyfile, "r", encoding="utf-8").readlines()[0].strip()
 
     c = Calendly(calendly_api_key)
-    s = Sheet(FLAGS.sheets_keyfile, FLAGS.sheet_id)
 
-    emails = s.get_range(FLAGS.email_range)['values']
-    apps = c.get_appointments()
+    apps = c.get_all_appointments()
 
-    for e in emails:
-        if e[0] in apps:
-            print(f"{e[0]}\t\t: {apps[e[0]]}")
-        else:
-            print(f"{e[0]}\t\tNo Appointment")
+    for a in apps:
+        print(f"{a},{apps[a][0]},{apps[a][1]},{apps[a][2]}")
 
 
 if __name__ == '__main__':
